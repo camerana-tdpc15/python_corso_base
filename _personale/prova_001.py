35b2542cb455172c24d8f2f4b0ec11598f572137
lamiera70 <lamiera70@yahoo.it>
2024-01-25 16:38:09 +0100

    aggiunto file prova_001.py

---
+++ _personale/prova_001.py
@@ -0,0 +1,24 @@
+
+
+'''
+
+It isn`t in the section 'C:\some\name_of_file'
+
+'''
+
+print("It isn`t in the section 'C:\some\\name_of_file'")
+
+
+def some_calculate(a, b):
+    print(abs((a // b) - pow(b, a)))
+
+
+some_calculate(10, 5)
+
+a = 10
+b = 5
+print(pow(b, a))
+print(a // b)
+print(a % b)
+
+# help("time.asctime") 
