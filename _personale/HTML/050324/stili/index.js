eb39af3b3b314a3f0b04c78069dd47e23f40a95c
tdpc15-05 <dretcanucristian@yahoo.com>
2024-03-12 18:46:17 +0100

    120324

---
+++ _personale/HTML/050324/stili/index.js
@@ -0,0 +1,82 @@
+
+
+console.log('questo e un file esterno');
+
+let firstName='pippo';
+
+//concatenazione di stringhe:
+console.log('nome utente: ' + firstName + '!'); //nome utente: poppi
+
+
+const lastName = 'Rossi'
+
+//backtick(digita alt+96  ``) 
+//template string:
+console.log(`cognomer utente: ${lastName}!`)
+
+
+console.log(`nome: ${firstName}, cognome:${lastName}`)
+
+
+let age = 60;
+console.log(age);
+
+console.log(1+1);  //2
+console.log(42 + age); //102
+
+let city ='';
+
+console.log(`citta: ${city}`);
+city= 'Torino'
+console.log(`&{city}`)
+
+let mediaVoti = [5, 3, 2, 4, 7];
+
+console.log(mediaVoti)
+
+let perdona= {
+    fName: 'Walter',
+    lName:'White',
+    isAlive: false,
+    age: 60,
+    work: ['inisegnante di chimica', 'cuoco']
+}
+const book= {
+    id: 1,
+    titolo: 'il miglio verde',
+    autore: {
+        nome:'222',
+        cognome: '333'},
+
+        numeroPagina: 656,  
+        generi:['giallo', 'thriller']
+    }
+
+    const auto= {
+        id: 42,
+        brand: 'FIAT',
+        modello: 'panda',
+        colore: 'giallo',
+        alimentazione: 'benzina', 'gpl',
+        targa: 'FX770NE',
+        proprietario: {
+            nome: 'aa',
+            cognome: 'bb',
+            residenza: [
+                
+                {
+                    city: 'Torino',
+                    cap: 10100
+                },
+                {
+                    city: 'Roma',
+                    cap: 10000
+                }
+            ]
+
+        }
+    }
+    console.log(auto);
+    console.log(auto.targa);
+    console.log(auto.alimentazione[0])
+
