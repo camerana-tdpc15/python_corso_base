0805db872d0e33710763d18f89834952410ba785
tdpc15-05 <dretcanucristian@yahoo.com>
2024-03-12 18:42:12 +0100

    040324

---
+++ _personale/HTML/260224/040324/script/index.js
@@ -0,0 +1,43 @@
+console.log('inizio file...');
+//questo e un comento su una linea
+/*  
+    questo e un
+    comento 
+    multilinea
+*/
+
+//stringa
+let firstName='Walter';
+var lastName='White';
+
+//number
+var age=60;
+var average=5.5;
+
+//bollean
+var isAlive=true;
+
+//valiri spciali
+var x=null;
+var y=undefined;
+
+firstName=42.123;
+
+if (age >=18) {
+    console.log('maggiorene');
+}
+else{
+    console.log('minorene)');
+}
+
+
+//  const= noon puo essere modificata
+
+//  let= puo essere modificata
+
+
+
+
+
+
+console.log('qui lo scripst finisce')    
