bc3b1a604fe360460a639305e0691bb4d1286da4
tdpc15-05_home <dretcanucristoan@yahoo.com>
2024-03-24 21:26:56 +0100

    240324

---
+++ _personale/HTML/240324_home/script_home_240324/index240324_home.js
@@ -0,0 +1,14 @@
+console.log('Starting console...')
+
+
+function esempio01 (p1, p2){return p1 * p2;}
+
+let result = esempio01(4, 3);
+document.getElementById('p01').innerHTML=result;
+
+
+
+function esempio02 (p1, p2, p3, p4){return (p1 + p2) / p3 - p4;}
+
+let result01 = esempio02 (10, 20, 2, 5);
+document.getElementById('p02').innerHTML=result01;
