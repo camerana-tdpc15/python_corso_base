9d7d5d6612d26ef3e6a970697c722abcecb71682
tdpc15-05 <dretcanucristian@yahoo.com>
2024-03-25 21:11:30 +0100

    250324

---
+++ _personale/HTML/250324_school/script/esercizio_lisra_spesa_con_array.js
@@ -0,0 +1,70 @@
+//alert('Ciao');
+console.log ('Starting...');
+
+/*
+variabili per input, button,ul,
+array per lista (aggiunge voce lista spessa)
+ */
+
+
+/*
+let nome = '42'
+if(nome == '42'){
+        alert('OK')
+
+        !!! TRIPLO =   === VERIFICA IL CINTENUTO E IL TIPO DI DATO
+}
+*/ 
+
+
+const inputListaSpessa =document.getElementById('Input-voce-lista');
+const btnInserisci=document.getElementsByClassName('btnInserisci')[0];
+const ulSpessa=document.getElementsByTagName('ul')[0];
+/* 
+con [0] li dico di prendere solo il primi elemento con class btn... e ul
+li altri non li prende in considerazione
+*/ 
+
+ let listaSpessa = []; //array da utilisare dopo
+
+//ci serve la gestione degli eventi
+btnInserisci.onclick =function() {
+    const item = inputListaSpessa.value;
+
+    if(item==''){
+        alert('Non Hai Inserito Nulla')
+    }
+    else{listaSpessa.push(item);
+    console.log(listaSpessa);
+    stampaListaSpessa();
+    inputListaSpessa.value =''
+}
+
+    //SOLUZIONE 01
+// console.log('con il eventi');
+// const item =inputListaSpessa.value;
+// if(item==''){
+// alert('non hai iniserito nulla');
+// }
+// else{
+    
+//     console.log(item);
+//     //prendi da input il suo valore
+    
+//     ulSpessa.innerHTML += `
+//     <li>${item}</li>
+    
+//     `;
+//     inputListaSpessa.value = '';
+//     //dopo che ai inserito il input lui ritorna con al valore null, vuoto
+}
+
+function stampaListaSpessa () {
+    ulSpessa.innerHTML = '';
+for(l of  listaSpessa) {
+    ulSpessa.innerHTML+=`
+    <li>${l}</li>
+    `
+}
+}
+
