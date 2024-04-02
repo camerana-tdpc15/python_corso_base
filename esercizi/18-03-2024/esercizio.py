9b125058f871509dbb393660da414d5a56998d8a
Daniele Saccinto <Danielesaccinto@gmail.com>
2024-04-02 18:48:02 +0200

    aggiornamento

---
+++ esercizi/18-03-2024/esercizio.py
@@ -0,0 +1,13 @@
+# 1) I dati da stampare sono:
+
+fruit1 = 'Mela'
+fruit2 = 'Banana'
+fruit3 = 'Fragola'
+
+# Dovrebbe stampare: "Mela, Banana, Fragola. Questi sono solo alcuni frutti!"
+
+# 2) Scrivi di seguito il tuo codice:
+
+...
+print ( fruit1, fruit2, fruit3 , sep=', ' ,end='. Questi sono alcuni frutti!')
+# 3) Ora esegui questa cella di codice per verificare che il tuo codice sia corretto.
