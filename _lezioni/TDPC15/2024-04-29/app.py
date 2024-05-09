dd966fb63a0223e287334bada63780e02a2d3896
Paolo Ortalda <paoloortalda@gmail.com>
2024-05-09 18:34:06 +0200

    ok

--- _lezioni/TDPC15/2024-04-29/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")  # Lo slash indica la root del sito
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-app.run(debug=True)  # , port=6969)
