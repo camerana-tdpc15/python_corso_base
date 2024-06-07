88fba3d107c76296567ce1a2bc87125d4f877dff
mantowalt <mantowalter@gmail.com>
2024-06-07 21:23:35 +0200

    nuova lezione

--- _lezioni/TDPC15/2024-06-04/flask_login/app.py
+++ _lezioni/TDPC15/2024-06-04/flask_login/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
+# Impostiamo una chiave segreta (che vediamo solo noi sviluppatori)
+# La chiave servirà per crittografare e firmare i cookies
+app.secret_key = 'my_very_secret_key123'
 
 # Struttura dati per contenere gli utenti
 # (in futuro leggeremo questi dati direttamente da un database)
@@ -14,6 +17,7 @@ USERS = {
 def home():
     return render_template('home.html')
 
+
 # route per Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -24,26 +28,46 @@ def login():
         # Leggiamo la password (rx_password)
         rx_password = request.form.get('tx_password')
 
-        print('User:', rx_username)
-        print('Password:', rx_password)
+        # print('User:', rx_username)
+        # print('Password:', rx_password)
 
         # Per copntrollare se un utente è presente
         if rx_username in USERS:
             if rx_password == USERS[rx_username]:
+                session['username'] = rx_username
+
                 # ATTENZIONE: Notate che se restituite il template films.html
                 #             l'URL rimane il medesimo!
                 # return render_template('films.html')
 
                 # Bisogna invece fare un redirect:
                 return redirect(url_for('films'))
+                # Che equivale a sctivere direttamente
+                # return redirect('/films')
+                # ...ma questa scrittura è meno manutenibile
+                # in caso di ridenominazione della route
 
     return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('home'))
 
 
 # route per Films
 @app.route('/films')
 def films():
-    return render_template('films.html')
+    if 'username' in session:
+
+        films = [
+            {'title': 'Akira', 'image': '/static/akira.jpg'}
+        ]
+
+        return render_template('films.html', films=films)
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
