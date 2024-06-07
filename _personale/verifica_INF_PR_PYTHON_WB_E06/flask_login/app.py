9b86e4401b23578d092901a6d8adaf1848cfc805
Paolo Ortalda <paoloortalda@gmail.com>
2024-06-07 21:19:37 +0200

    esercizio_6

--- _personale/verifica_INF_PR_PYTHON_WB_E06/flask_login/app.py
+++ _personale/verifica_INF_PR_PYTHON_WB_E06/flask_login/app.py
@@ -1,48 +1,61 @@
-from flask import Flask, request, render_template, flash, session
+from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
+# impostiamo una chiave segreta per crittografare e firmare i cookies
+app.secret_key = 'my_very_secret_key123'
 
-# creo una lista di dizionari con nome utente e password
-users = {
-    'mrossi' : 'osoejfj3',
-    'ggangi' : 'odoeooeee'
+# Struttura dati per contenere gli utenti
+# (in futuro leggeremo questi dati direttamente da un database)
+USERS = {
+    'mrossi': 'osoejfj3',
+    'ggangi': 'odoeooeee'
 }
 
-
-# route per home
+# route per Home
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# route per login
+# route per Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Se il metodo è POST:
+    # Se il metodo è POST
     if request.method == 'POST':
-
-        # leggiamo il nome utente (rx_username)
-        # si usa 'request.form' perchè abbiamo i dati provenienti 
-        # da un form quindi 'FormData'
+        # Leggiamo il nome utente (rx_username)
         rx_username = request.form.get('tx_user')
-        # leggiamo la password
+        # Leggiamo la password (rx_password)
         rx_password = request.form.get('tx_password')
 
-        #print('user ',rx_username)
-        #print('password ', rx_password)
-            # per controllare se un utente è presente
-        if rx_username in users:
-            
-            if rx_password == users[rx_username]:
-                
-                return render_template('film.html')
-        
+        print('User:', rx_username)
+        print('Password:', rx_password)
+
+        # Per copntrollare se un utente è presente
+        if rx_username in USERS:
+            if rx_password == USERS[rx_username]:
+
+                session['username'] = rx_username
+                # ATTENZIONE: Notate che se restituite il template films.html
+                #             l'URL rimane il medesimo!
+                # return render_template('films.html')
+
+                # Bisogna invece fare un redirect:
+                return redirect(url_for('films'))
+
     return render_template('login.html')
 
-# route per film 
-@app.route('/film')
-def film():
-    return render_template('film.html')
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('home'))
 
-# questo si mette cosi sempre
-if __name__== '__main__':
+# route per Films
+@app.route('/films')
+def films():
+    if 'username' in session:
+        return render_template('films.html')
+    else:
+        return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
     app.run(debug=True)
