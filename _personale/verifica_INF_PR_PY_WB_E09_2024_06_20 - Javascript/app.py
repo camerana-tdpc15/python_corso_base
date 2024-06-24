0ec8b356abba3bdbb2369e567782e2aa0a7366ec
Paolo Ortalda <paoloortalda@gmail.com>
2024-06-24 14:58:03 +0200

    ok

---
+++ _personale/verifica_INF_PR_PY_WB_E09_2024_06_20 - Javascript/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template, jsonify, redirect
+import os
+
+app = Flask(__name__)
+
+# Ottiene il percorso assoluto della directory del file corrente
+BASE_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+
+# Definisce il percorso completo del file guestbook.txt
+GUESTBOOK_PATH = os.path.join(BASE_DIR_PATH, 'guestbook.txt')
+
+# Route per la homepage
+@app.route('/')
+def home():   
+    with open(GUESTBOOK_PATH, mode='r', encoding='utf-8') as file:
+        messaggi = file.readlines()
+    return render_template('guestbook.html', messaggi=messaggi)
+    
+# Route per gestire l'aggiunta di nuovi messaggi al guestbook
+@app.route('/api/guestbook', methods = ['POST'])
+def scrittura():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        messaggio = request.form.get('messaggio')
+        with open(GUESTBOOK_PATH, mode='a', encoding='utf-8') as file:
+            file.write(f'{nome} : {messaggio}\n')
+        return redirect('/')          
+
+    else:
+        with open(GUESTBOOK_PATH, mode='r', encoding='utf-8') as file:
+            messaggi = file.readlines()
+            
+        messaggi_json = jsonify(messaggi)
+        return messaggi_json
+        
+
+
+# Avvia l'applicazione Flask in modalità debug
+if __name__ == '__main__':
+    app.run(debug=True)
