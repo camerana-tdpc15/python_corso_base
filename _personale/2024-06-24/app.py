e70fcf087411cb7c5d00206f2f49f7b6d7d2e373
lamiera70 <lamiera70@yahoo.it>
2024-06-24 09:45:15 +0200

    esercizio WB_E09 parte uno

---
+++ _personale/2024-06-24/app.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+
+BASE_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+MIO_FILE_PATH = os.path.join(BASE_DIR_PATH, "guestbook.txt")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("guestbook.html")
+
+
+@app.route("/api/guestbook", methods=["GET", "POST"])
+def login():
+    # Se il metodo è POST
+    if request.method == "POST":
+        # Leggiamo il nome utente (rx_username)
+        rx_nome = request.json.get("nome")
+        # Leggiamo la password (rx_password)
+        rx_messaggio = request.json.get("messaggio")
+
+        if not rx_nome or not rx_messaggio:
+            response = {"error": "nome e messaggio sono obbligatori"}
+
+        # Per copntrollare se un utente è presente
+
+        with open(MIO_FILE_PATH, mode="a", encoding="utf-8") as file:
+            file.write(f"{rx_nome} {rx_messaggio}\n")
+            response = {"success": "ok"}
+        return jsonify(response)
+
+    else:
+        if os.path.exists(MIO_FILE_PATH):
+            print("...............ci sono")
+
+            with open(MIO_FILE_PATH, mode="r", encoding="utf-8") as file:
+                fx_messaggio = file.readlines()
+
+        return jsonify(fx_messaggio)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
