95ded2082e6de18c796476257bad23b42b405a36
Eduardo <eduardobambace@gmail.com>
2024-07-02 12:10:12 +0200

    il-mio-change

--- _personale/E11/app.py
+++ _personale/E11/app.py
@@ -1,32 +1,84 @@
 from flask import Flask, request, render_template, redirect, url_for, session, flash
-from settings import DATABASE
-from models import init_db, db, users, lotti,prenotazioni,prodotti,produttori
-
+from settings import DATABASE_PATH
+from models import init_db, db,User
 app = Flask(__name__)
 
 app.config.update(
     SECRET_KEY='my_very_secret_key123',
-    SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE,  # Il path al database
+    SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE_PATH,  # Il path al database
     DEBUG=True  # Imposto qua la modalità debug
                 # Vedi app.run() alla fine del file
 )
 
-db.init_app(app)  # Inizializza l'istanza di SQLAlchemy con l'app Flask
+
+
+PRODOTTI = [
+    {'title': 'Farina di grano tenero', 'image': 'fgt.jpeg'},
+    {'title': 'Formaggio toma', 'image': 'ft.jpg'},
+    {'title': 'Mele Golden', 'image': 'mg.jpeg'},
+    {'title': 'Miele di acacia Bio', 'image': 'mda.jpeg'},
+    {'title': 'Olio Extravergine di oliva Bio', 'image': 'odo.jpeg'},
+    {'title': 'Riso Integrale', 'image': 'ri.jpeg'},
+    {'title': 'Zucchine novelle ', 'image': 'zn.jpeg'},
+]
 
 
 @app.route('/')
-def home():
+def index():
     
+    return render_template('index.html', prodotti = PRODOTTI)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if not  username or not password:
+            flash('Tutti i campi sono obbligatori!')
+            return redirect(url_for('signup'))
+        if User.query.filter_by(username=username).first() :
+            flash("Il nickname o l'username sono già in uso!")
+            return redirect(url_for('signup'))
+        new_user = User( username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registrazione effettuata con successo!')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+
+@app.route('/home')
+def home():
+    if 'user_id' in session:
+        user = db.session.query(user).get(session['user_id'])
+        return render_template('home.html', utente=user)
     return render_template('home.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            session['user_id'] = user.id
+            flash('Login riuscito!')
+            return redirect(url_for('guestbook'))
+        else:
+            flash('Credenziali non valide!')
+            return redirect(url_for('login'))
+    return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('Logout effettuato con successo!')
+    return redirect(url_for('home'))
 
-
-
+db.init_app(app)  # Inizializza l'istanza di SQLAlchemy con l'app Flask
 
 if __name__ == '__main__':
     with app.app_context():
         init_db(app)
-    app.run()  # Notate che ho rimosso il parametro `debug=True` perché ora è
-               # impostato in `app.config`. Questo perché altriment la funzione
-               # init_db si ritroverebbe il livello di logging non impostato (0).
+    app.run(debug=True)
--- _personale/e11-JSON/models.py
+++ _personale/e11-JSON/models.py
@@ -1,199 +1,83 @@
-import csv
-import os
-import sys
 from flask_sqlalchemy import SQLAlchemy
-from settings import USERS_TABLE_NAME,LOTTI_TABLE_NAME, PRENOTAZIONI_TABLE_NAME, PRODOTTI_TABLE_NAME,PRODUTTORI_TABLE_NAME,USERS_TABLE_CSV,LOTTI_TABLE_CSV,PRENOTAZIONI_TABLE_CSV,PRODOTTI_TABLE_CSV,PRODUTTORI_TABLE_CSV
+import os
+import csv
+import sys
+
 
 db = SQLAlchemy()  # Crea l'istanza di SQLAlchemy
 
 
-
-class users(db.Model):
-    __tablename__ = USERS_TABLE_NAME
-    id = db.Column(db.Integer, primary_key=True)
-    cognome = db.Column(db.String(50),nullable=False)
-    nome = db.Column(db.String(50),nullable=False)
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50), nullable=False)
+    cognome = db.Column(db.String(50), nullable=False)
     telefono = db.Column(db.String(20))
-    email = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
 
-class lotti(db.Model):
-    __tablename__ = LOTTI_TABLE_NAME
-    id = db.Column(db.Integer, primary_key=True)
-    prodotto_id = db.Column(db.String(80),db.ForeginKey('prodotti.id')) #fk
-    data_consegna = db.Column(db.String(80))
-    qta_unita_misura = db.Column(db.String(20))
-    qta_lotto = db.Column(db.String(100))
-    prezzo_unitario=db.Column(db.Integer)
-    sospeso = db.Column(db.String())
+class Produttore(db.Model):
+    __tablename__ = 'produttori'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_produttore = db.Column(db.String(), unique=True, nullable=False)
+    descrizione = db.Column(db.Text(), nullable=False)
+    indirizzo = db.Column(db.Text(), nullable=False)
+    telefono = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
 
-class prenotazioni(db.Model):
-    __tablename__ = PRENOTAZIONI_TABLE_NAME
-    id = db.Column(db.Integer, primary_key=True)
-    utente_id= db.Column(db.Integer,db.ForeginKey('users.id')) #fk
-    lotto_id = db.Column(db.Integer,db.ForeginKey('lotti.id')) #fk
-    qta = db.Column(db.Integer)
+class Prodotto(db.Model):
+    __tablename__ = 'prodotti'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    produttore_id = db.Column(db.Integer, db.ForeignKey('produttori.id'), nullable=False)
+    nome_prodotto = db.Column(db.String(50), nullable=False)
 
-
-class prodotti(db.Model):
-    __tablename__ = PRODOTTI_TABLE_NAME
-    id = db.Column(db.Integer, primary_key=True, autoincremet=True)
-    produttori_id= db.Column(db.Integer,db.ForeginKey('produttori.id'), unique=True) #fk
-    nome_prodotto = db.Column(db.String(80))
- 
-
-class produttori(db.Model):
-    __tablename__ = PRODUTTORI_TABLE_NAME
-    id = db.Column(db.Integer, primary_key=True)
-    nome_produttore= db.Column(db.String(),unique=True) 
-    descrizione = db.Column(db.Text(),nullable=False) 
-    indirizzo = db.Column(db.Text(),nullable=False)
-    telefono = db.Column(db.Text(),nullable=False)
-    email = db.Column(db.String(), nullable = False)
+class Lotto(db.Model):
+    __tablename__ = 'lotti'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    prodotto_id = db.Column(db.Integer, db.ForeignKey('prodotti.id'), nullable=False)
+    data_consegna = db.Column(db.Date, nullable=False)
+    qta_unita_misura = db.Column(db.String(10), nullable=False)
+    qta_lotto = db.Column(db.Integer, nullable=False)
+    prezzo_unitario = db.Column(db.Float, nullable=False)
+    sospeso = db.Column(db.Boolean, default=False)
 
 
-
+class Prenotazione(db.Model):
+    __tablename__ = 'prenotazioni'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    lotto_id = db.Column(db.Integer, db.ForeignKey('lotti.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    qta = db.Column(db.Integer, nullable=False)
 
 
 
 def init_db(app):
-    """
-    Funzione per inizializzare il database e popolare le tabelle con i dati
-    presenti nei file CSV.
-    :param app: Applicazione Flask corrente (per accedere al logger)
-    """
-
-    # Crea le tabelle nel database, se queste non esistono già
+    # Crea le tabelle se non esistono già
     db.create_all()
 
-    # Se la tabella NON è già popolata
-    if not users.query.first():
-        # Se il file CSV esiste
-        if os.path.exists(USERS_TABLE_CSV):
-            # Apre il file CSV in lettura
-            with open(USERS_TABLE_CSV, 'r') as csv_file:
-                # Legge il file CSV come dizionario
-                csv_reader = csv.DictReader(csv_file)
-                # Per ogni riga del file CSV
-                for row in csv_reader:
-                    # Crea un oggetto User con i valori della riga corrente
-                    new_record = users(
-                        cognome=row['cognome'],
+    if not User.query.first():
+
+        if os.path.exists('users'):
+
+            with open('users','r') as csv_file:
+
+                csv_reader= csv.DictReader(csv_file)
+
+                for row  in csv_reader:
+
+                    new_record= User(
                         nome=row['nome'],
+                        cognome=row['cognome'],
                         telefono=row['telefono'],
                         email=row['email'],
-                        password = row['password']
+                        password=row['password'],
                     )
-                    # Aggiunge l'oggetto User al database
-                    db.session.add(new_record)
-                # Conferma le modifiche al database
-                db.session.commit()
-                app.logger.info(f'Tabella "{USERS_TABLE_NAME}" popolata correttamente.')
-        else:
-            app.logger.error(
-                f'Il file "{USERS_TABLE_CSV}" non esiste. '
-                'Verifica il percorso e riprova.'
-            )
-            sys.exit(1)
-    else:
-        app.logger.info(f'Tabella "{USERS_TABLE_NAME}" già popolata.')
 
-    # Idem come sopra, ma per la tabella lotti
-    if not lotti.query.first():
-        if os.path.exists(LOTTI_TABLE_CSV):
-            with open(LOTTI_TABLE_CSV, 'r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    new_record = lotti(
-                        prodotto_id=row['prodotto_id'],
-                        data_consegna=row['data_consegna'],
-                        qta_unita_misura = row['qta_unita_misura'],
-                        qta_lotto=row['qta_lotto'],
-                        prezzo_unitario=row['prezzo_unitario'],
-                        sospeso = row['sospeso'],
-                    )
                     db.session.add(new_record)
                 db.session.commit()
-                app.logger.info(f'Tabella "{LOTTI_TABLE_NAME}" popolata correttamente.')
+                app.logger.info(f'Tabella Users popolata correttamente')
         else:
-            app.logger.error(
-               f'Il file "{LOTTI_TABLE_CSV}" non esiste. '
-               'Verifica il percorso e riprova.'
-            )
+            app.logger.error(f'Tabella Users non esiste, Verifica il percorso e riprova')
             sys.exit(1)
     else:
-        app.logger.info(f'Tabella "{LOTTI_TABLE_NAME}" già popolata.')
-
-           # Idem come sopra, ma per la tabella prenottazioni
-    if not prenotazioni.query.first():
-        if os.path.exists(PRENOTAZIONI_TABLE_CSV):
-            with open(PRENOTAZIONI_TABLE_CSV, 'r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    new_record = prenotazioni(
-                        utente_id=row['utente_id'],
-                        lotto_id=row['lotto_id'],
-                        qta = row['qta'],
-                    )
-                    db.session.add(new_record)
-                db.session.commit()
-                app.logger.info(f'Tabella "{PRENOTAZIONI_TABLE_NAME}" popolata correttamente.')
-        else:
-            app.logger.error(
-               f'Il file "{PRENOTAZIONI_TABLE_CSV}" non esiste. '
-               'Verifica il percorso e riprova.'
-            )
-            sys.exit(1)
-    else:
-        app.logger.info(f'Tabella "{PRENOTAZIONI_TABLE_NAME}" già popolata.')
-
-
-
-     # Idem come sopra, ma per la tabella prodotti
-    if not prodotti.query.first():
-        if os.path.exists(PRODOTTI_TABLE_CSV):
-            with open(PRODOTTI_TABLE_CSV, 'r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    new_record = prodotti(
-                        produttore_id=row['produttore_id'],
-                        nome_prodotto=row['nome_prodotto'],
-                    )
-                    db.session.add(new_record)
-                db.session.commit()
-                app.logger.info(f'Tabella "{PRODOTTI_TABLE_NAME}" popolata correttamente.')
-        else:
-            app.logger.error(
-               f'Il file "{PRODOTTI_TABLE_CSV}" non esiste. '
-               'Verifica il percorso e riprova.'
-            )
-            sys.exit(1)
-    else:
-        app.logger.info(f'Tabella "{PRODOTTI_TABLE_NAME}" già popolata.')
-
-
-
-         # Idem come sopra, ma per la tabella produttori
-    if not produttori.query.first():
-        if os.path.exists(PRODUTTORI_TABLE_CSV):
-            with open(PRODUTTORI_TABLE_CSV, 'r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    new_record = produttori(
-                        nome_produttore=row['nome_produttore'],
-                        descrizione=row['descrizione'],
-                        indirizzo=row['indirizzo'],
-                        telefono=row['telefono'],
-                        email =row['email']
-                    )
-                    db.session.add(new_record)
-                db.session.commit()
-                app.logger.info(f'Tabella "{PRODUTTORI_TABLE_NAME}" popolata correttamente.')
-        else:
-            app.logger.error(
-               f'Il file "{PRODUTTORI_TABLE_CSV}" non esiste. '
-               'Verifica il percorso e riprova.'
-            )
-            sys.exit(1)
-    else:
-        app.logger.info(f'Tabella "{PRODUTTORI_TABLE_NAME}" già popolata.')
+        app.logger.info(f'Tabella Users già popolata.')
