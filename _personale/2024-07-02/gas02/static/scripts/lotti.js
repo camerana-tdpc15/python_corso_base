8bc718e1f7c7091b4bf98206e6a33cb85ee0d25a
lamiera70 <lamiera70@yahoo.it>
2024-07-02 21:22:00 +0200

    aggiunto

---
+++ _personale/2024-07-02/gas02/static/scripts/lotti.js
@@ -0,0 +1,52 @@
+// alert('ok');
+
+const rowLotti = document.querySelector('#row-lotti');
+
+
+// Fa fetch di un file JSON e lo stampa in console
+  fetch("../static/data/dati_lotti.json")
+  .then(response => response.json())
+  .then(data => {
+      for (lotto of data){
+          //console.log(lotto);
+
+          let displayButton = '';
+          
+          if(lotto.sospeso) {
+            // button rosso
+            displayButton = '<button class="btn btn-danger w-100" disabled>Sospeso</button>';
+          }
+          else if (lotto.get_qta_disponibile == 0) {
+              displayButton = '<button class="btn btn-warning w-100" disabled>Esaurito</button>';  
+            }
+          else {
+             displayButton = '<button class="btn btn-primary w-100">Prenota</button>';  
+             
+          }
+        
+        
+
+          rowLotti.innerHTML += `
+          
+          <div class="col-lg-3 my-2">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h4 class="card-title">${lotto.prodotto.nome}</h4>
+                        <p class="text-end"><small>(cod. lotto: ${lotto.id})</small></p>
+                    </div>
+                    <div class="card-body">
+                        <p>Produttore: <b>${lotto.prodotto.produttore.nome}</b></p>
+                        <p>Data consegna: <b>${lotto.data_consegna}</b></p>
+                        <p>Qtà Tot: <b>${lotto.qta_lotto}</b></p>
+                        <p>Qtà Disp: <b>${lotto.get_qta_disponibile}</b></p>
+                        <p>Prezzo: <b>${lotto.get_prezzo_str}</b></p>
+                        ${displayButton}
+                    </div>
+                </div>          
+          </div>
+          
+          `;
+      }
+
+  
+  });
