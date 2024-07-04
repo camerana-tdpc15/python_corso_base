e3c991ca0c2189f27eebcb13e3bfbaa8f2616097
mantowalt <mantowalter@gmail.com>
2024-07-04 21:30:22 +0200

    nuova lezione

--- _lezioni/TDPC15/2024-07-04/gas02/models.py
+++ _lezioni/TDPC15/2024-07-04/gas02/models.py
@@ -1,4 +1,9 @@
+import os
+import json
+from datetime import date
+from pprint import pprint
 from flask_sqlalchemy import SQLAlchemy
+from settings import BASE_DIR
 
 db = SQLAlchemy()
 
@@ -10,6 +15,7 @@ class User(db.Model):
     telefono = db.Column(db.String(20))
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
+
 
 class Produttore(db.Model):
     __tablename__ = 'produttori'
@@ -51,5 +57,33 @@ def init_db():
     # Crea le tabelle se non esistono già
     db.create_all()
 
-    # Popolo le tabelle con i dati
-    ...
+    # Popolo le tabelle con i dati se non esiste un record in User
+    if User.query.first() is None:
+        # 
+        json_files = [
+            ('lotti.json', Lotto),
+            ('prenotazioni.json', Prenotazione),
+            ('prodotti.json', Prodotto),
+            ('produttori.json', Produttore),
+            ('users.json', User),
+        ]
+
+        for filename, model in json_files:
+            file_path = os.path.join(BASE_DIR, 'database', 'data_json', filename)
+            # print(file_path)
+
+            with open(file_path, 'r') as file:
+                
+                lista_record = json.load(file)
+
+            for record_dict in lista_record:
+
+                if 'data_consegna' in record_dict:
+                    var_data_consegna = date.fromisoformat(record_dict['data_consegna'])
+                    record_dict['data_consegna'] = var_data_consegna
+
+                new_record = model(**record_dict)
+
+                db.session.add(new_record)
+            
+        db.session.commit()
