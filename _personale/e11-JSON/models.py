81cc461f14a393c36f903f5aac4a9810326f2970
tdpc15-01 <eduardobambace@gmail.com>
2024-07-05 21:30:01 +0200

    ...

--- _personale/e11-JSON/models.py
+++ _personale/e11-JSON/models.py
@@ -63,6 +63,8 @@ class Lotto(db.Model,SerializerMixin):
     #RELATIONSHIPS
     rel_prodotto = db.relationship('Prodotto',back_populates='rel_lotti')
     rel_prenotazioni= db.relationship('Prenotazione', back_populates='rel_lotto')
+
+    serialize_rules = (' -rel.prodotto.rel_lotti','get_date','get_prezzo_str','get_qta_disponibile')
     
     def get_date(self): #quando siamo dentro una clase e definiamo una funzione d'istanza, devono avere self, rapresenta il record reale da manipolare
        res_data = self.data_consegna.strftime('%A %d/%m/%Y')
