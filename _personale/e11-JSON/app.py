fef3fb318a54c93eff1c2e35c20c29dd3d69ca40
tdpc15-01 <eduardobambace@gmail.com>
2024-07-05 21:22:21 +0200

    ...

--- _personale/e11-JSON/app.py
+++ _personale/e11-JSON/app.py
@@ -1,15 +1,63 @@
-from flask import Flask, render_template
-from models import db, init_db
+from flask import Flask, render_template,request,session,flash,redirect,url_for,jsonify
+from models import db, init_db,Lotto,Prodotto,Produttore
 from settings import DATABASE_PATH
+import locale
+
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+DATABASE_PATH
 
 db.init_app(app)  # Inizializza l'istanza di SQLAlchemy con l'app Flask
 
+
+
+locale.setlocale(locale.LC_TIME,'it_IT')
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/api/lotti', methods=['GET'])#non e necesario specificare se e soltanto GET
+def get_lotti():
+
+    lotti = Lotto.query.all()
+
+
+# para serializar una estructura usar una libreria
+#proviamo a mano
+    lotti_data = []
+
+    for lotto in lotti:
+      
+        prodotto = db.session.get(Prodotto,lotto.prodotto_id)
+        produttore=db.session.get(Produttore,prodotto.produttore_id)
+
+        data = {
+            'id':lotto.id,
+            'data_consegna':lotto.data_consegna,
+            'get_date':lotto.get_date(),
+            'get_qta_disponibile':lotto.get_qta_disponibile(),
+            'qta_unita_misura':lotto.qta_unita_misura,
+            'qta_lotto':lotto.qta_lotto,
+            'prezzo_unitario':lotto.prezzo_unitario,
+            'sospeso':lotto.sospeso,
+            #'prodotto_nome' : prodotto.nome_prodotto => questa e UNA soluzione
+            #COSI E PIU LEGIBILE E VA BENE CON QUELLO CHE ABBIAMO FATTO
+            'prodotto':{
+                'prodotto_nome' : prodotto.nome_prodotto,
+                'produttore':{ 
+                    'nome_produttore':produttore.nome_produttore}
+            }
+        }
+
+        lotti_data.append(data)
+    
+    return jsonify(lotti_data)
+
+
+
 
 
 # @TODO: Qua ci vanno le routes
--- _personale/e11-JSON/models.py
+++ _personale/e11-JSON/models.py
@@ -1,9 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from settings import BASE_DIR
-from pprint import pprint
-from datetime import date
 import os
 import json
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+from pprint import pprint
+from  sqlalchemy_serializer import SerializerMixin
+from settings import BASE_DIR
 
 db = SQLAlchemy()
 
@@ -30,7 +31,7 @@ class User(db.Model):
 
 
 
-class Produttore(db.Model):
+class Produttore(db.Model,SerializerMixin):
     __tablename__ = 'produttori'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome_produttore = db.Column(db.String(), unique=True, nullable=False)
@@ -39,13 +40,17 @@ class Produttore(db.Model):
     telefono = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=False)
 
-class Prodotto(db.Model):
+class Prodotto(db.Model,SerializerMixin):
     __tablename__ = 'prodotti'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     produttore_id = db.Column(db.Integer, db.ForeignKey('produttori.id'), nullable=False)
     nome_prodotto = db.Column(db.String(50), nullable=False)
 
-class Lotto(db.Model):
+    #RELATIONSHIPS
+    rel_lotti = db.relationship('Lotto',back_populates='rel_prodotto')
+   
+
+class Lotto(db.Model,SerializerMixin):
     __tablename__ = 'lotti'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     prodotto_id = db.Column(db.Integer, db.ForeignKey('prodotti.id'), nullable=False)
@@ -55,16 +60,39 @@ class Lotto(db.Model):
     prezzo_unitario = db.Column(db.Float, nullable=False)
     sospeso = db.Column(db.Boolean, default=False)
 
+    #RELATIONSHIPS
+    rel_prodotto = db.relationship('Prodotto',back_populates='rel_lotti')
+    rel_prenotazioni= db.relationship('Prenotazione', back_populates='rel_lotto')
+    
+    def get_date(self): #quando siamo dentro una clase e definiamo una funzione d'istanza, devono avere self, rapresenta il record reale da manipolare
+       res_data = self.data_consegna.strftime('%A %d/%m/%Y')
+       return res_data
+    
+    def get_prezzo_str(self):
+        return f'{self.prezzo_unitario} €/  {self.qta_unita_misura}'
+    
 
-class Prenotazione(db.Model):
+    def get_qta_disponibile(self):
+        qta_prenotata = 0
+
+        for prenot in  self.rel_prenotazioni:
+            qta_prenotata +=  prenot.qta
+
+        return self.qta_lotto - qta_prenotata
+
+class Prenotazione(db.Model,SerializerMixin):
     __tablename__ = 'prenotazioni'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     lotto_id = db.Column(db.Integer, db.ForeignKey('lotti.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     qta = db.Column(db.Integer, nullable=False)
 
-    # @TODO: Da implementare l'unique constraint per la coppia lotto_id e user_id
-    # ...
+
+    #RELATIONSHIP
+    rel_lotto= db.relationship('Lotto', back_populates='rel_prenotazioni')
+
+
+ 
 
 def init_db():
     # Crea le tabelle se non esistono già
@@ -98,7 +126,7 @@ def init_db():
                     data_consegna = date.fromisoformat(record_dict['data_consegna'])
                     record_dict['data_consegna']= data_consegna
 
-                new_user = model(**record_dict) # solo en json seusa el doble asterisco, para la llave y valor  
+                new_user = model(**record_dict) # solo en json se usa el doble asterisco, para la llave y valor  
 
 
                 db.session.add(new_user)
