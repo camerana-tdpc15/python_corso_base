d50f89fed87f9dc2aa112c9a6eefc59abce64ab8
lamiera70 <lamiera70@yahoo.it>
2024-07-08 21:32:14 +0200

    aggiunto

--- _personale/2024-07-01_NEW_LOGIN_001/gas/app.py
+++ _personale/2024-07-01_NEW_LOGIN_001/gas/app.py
@@ -20,7 +20,7 @@ db.init_app(app)  # Inizializza l'istanza di SQLAlchemy con l'app Flask
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', )
 
 
 
