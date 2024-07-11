dad856f535efdd83ddbfed0dfac9d0683883b61b
RC <riccardocoda@yahoo.it>
2024-07-11 18:07:54 +0200

    finito esercizio 09

--- _lezioni/TDPC15/202040620_esercitazione/app.py
+++ _lezioni/TDPC15/202040620_esercitazione/app.py
@@ -1,17 +1,19 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, url_for
+from markupsafe import escape
 
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 HOME = os.path.join(BASE_DIR, "templates", "home.html")
+DB_FILE_PATH = os.path.join(BASE_DIR, 'guestbook.txt')
 
 app = Flask(__name__)
 
 
 
-@app.route("/")  # Lo slash indica la root del sito
+@app.route("/")  
 def render_home():
     return  render_template('home.html')
 
@@ -22,23 +24,41 @@ def guestbook():
     if request.method == 'POST':
         data = request.json
 
-        print(data)
-
-
-
         # Lettura di nome e messaggio dalla request
+        # stama di prova sul terminale di corretta ricezione 
     
-
+        print(data)
         nome = data['nome']
         messaggio = data['messaggio']
-
         print(nome)
         print(messaggio)
 
+        if not nome or not messaggio:
+            response = {'error': 'Nome e messaggio sono obbligatori!'}
+        else:
+            # SCRITTURA SU FILE: Apertura del file in modalità append
+            with open(DB_FILE_PATH, mode='a', encoding='utf-8') as file:
+                
+                file.write(f'{escape(nome)}: {escape(messaggio)}\n')
+            response = {'success': True}
+
+        return jsonify(response)
+
         
     else:
-       data = request.form
-       print(data)
+        if os.path.exists(DB_FILE_PATH):
+            with open(DB_FILE_PATH, mode='r', encoding='utf-8') as file:
+                message_to_send = file.readlines()
+
+        else:
+            message_to_send = ['non ci sono ancora messaggi :_(<br>vuoi essere il primo?']
+        
+        return jsonify(message_to_send)
+
+
+                
+
+     
         
 
 
--- _lezioni/TDPC15/202040620_esercitazione/static/script.js
+++ _lezioni/TDPC15/202040620_esercitazione/static/script.js
@@ -1,22 +1,52 @@
 console.log('ciao')
 
 const inputButton = document.getElementById('button');
+const spazioPerTesto = document.getElementById('testoDaMostrare');
 
-inputButton.addEventListener('click', sendMessage);
 
+inputButton.onclick = function() {sendMessage()};
+
+
+document.addEventListener('DOMContentLoaded', getMessage);
+
+function getMessage() {
+    fetch('/api/guestbook').then(response => response.json())
+    .then(messages => {
+
+        spazioPerTesto.innerHTML ='';
+
+        messages.forEach(element => {
+            spazioPerTesto.innerHTML += `<li>${element}</li>`;
+        });
+    }).catch(error => console.error('Error:', error));
+};
 
 function sendMessage() {
     let data = {
         'nome': document.getElementById('name').value,
         'messaggio': document.getElementById('message').value,
     };
-    // Invia la richiesta al server
+    
     fetch('/api/guestbook', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
-    })
-    .then(response => response.json());
-    let form = getElementById('msg-form');
-    form.reset();
-}
+    }).then(response => response.json())
+    .then(result => {
+
+        if (result.success) {
+            getMessage();
+
+            let form = getElementById('msg-form');
+            form.reset();
+        }
+
+        else if (result.error){alert(result.error)}
+        else {alert('errore')};
+
+
+
+    }).catch(error => console.error('Errore: ', error));
+    
+};
+
