c5471dc164732ffb26d9fa7e5f43b46ae5ff07a2
RC <riccardocoda@yahoo.it>
2024-07-15 17:07:49 +0200

    MODIFICHE

--- _personale/A_PROVA_FINALE/app.py
+++ _personale/A_PROVA_FINALE/app.py
@@ -1,5 +1,22 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, session, flash
-from models import User, Lotto, Prenotazione, Prodotto, Produttore, db
-from populate_db import init_db
+from flask import Flask, session
+from models import   db, init_db
+
+# Lotto, Prenotazione, Prodotto, Produttore, User,
+
 from settings import DATABASE_PATH
 
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE_PATH
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return ('CIAO')
+
+if __name__ == '__main__':
+    with app.app_context():
+        init_db()
+    app.run(debug=True)
--- _personale/A_PROVA_FINALE/models.py
+++ _personale/A_PROVA_FINALE/models.py
@@ -16,7 +16,80 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False) 
 
-    serialize_rules = ('-password',  '-pronotazioni.user')
+    serialize_rules = ('-password',)
+
+class Produttore(db.Model, SerializerMixin):
+    __tablename__ = 'produttori'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_produttore = db.Column(db.String(100), unique=True, nullable=False)
+    descrizione = db.Column(db.String(50), nullable=False)
+    indirizzo = db.Column(db.String(100), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+   
+   
+
+class Prodotto(db.Model, SerializerMixin):
+    __tablename__ = 'prodotti'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    produttore_id = db.Column(db.Integer, db.ForeignKey('produttori.id'), nullable=False)
+    nome_prodotto = db.Column(db.String(50), nullable=False)
+
+class Prenotazione(db.Model, SerializerMixin):
+    __tablename__ = 'prenotazioni'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    utente_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    lotto_id = db.Column(db.Integer, db.ForeignKey('lotti.id'), nullable=False)
+    qta = db.Column(db.Integer, nullable=False)
+
+
+
+
+class Lotto(db.Model, SerializerMixin):
+    __tablename__ = 'lotti'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    prodotto_id = db.Column(db.Integer, db.ForeignKey('prodotti.id'), nullable=False)
+    data_consegna = db.Column(db.Date, nullable=False)
+    qta_unita_misura= db.Column(db.String(10), nullable=False)
+    qta_lotto = db.Column(db.Integer, nullable=False)
+    prezzo_unitario = db.Column(db.Float, nullable=False)
+    sospeso = db.Column(db.Boolean, nullable=False)
+
+
+def init_db():
+    
+    db.create_all()
+    
+    if User.query.first() is None:
+        json_files = [
+            ('lotti.json', Lotto),
+            ('prenotazioni.json', Prenotazione ),
+            ('prodotti.json', Prodotto ),
+            ('produttori.json', Produttore ),
+            ('users.json', User ),
+        ]
+
+        for filename, model in json_files:
+            filepath = os.path.join(DATA_PATH, filename)
+
+            with open (filepath, 'r') as file:
+                lista_record = json.load(file)
+
+            for record_dict in lista_record:
+                if 'data_consegna'in record_dict:
+                    var_data_consegna = date.fromisoformat(record_dict['data_consegna'])
+                    record_dict['data_consegna'] = var_data_consegna  
+                
+                new_record = model(**record_dict)
+                db.session.add(new_record)
+        db.session.commit()
+            
+
+        
+        
+
+
+
 
 
 
