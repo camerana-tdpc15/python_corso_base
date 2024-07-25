5eedde527b0110d0adf6a06dea7327cb06af1737
lamiera70 <lamiera70@yahoo.it>
2024-07-25 15:28:23 +0200

    aggiunto

--- _personale/2024-07-15_ESAME/static/scripts/load_repliche.js
+++ _personale/2024-07-15_ESAME/static/scripts/load_repliche.js
@@ -12,21 +12,7 @@ fetch("/api/repliche?order=asc")
         for (replica of data) {
             console.log(replica);
 
-            // debugger;
-
-            // let displayButton = '';
-            // if(replica.annullato) {
-            //     // button rosso
-            //     displayButton = '<button class="btn btn-danger w-100" disabled>Sospeso</button>';
-            // }
-            // else if (replica.get_qta_disponibile == 0) {
-            //     // button giallo
-            //     displayButton = '<button class="btn btn-warning w-100" disabled>Esaurito</button>';
-            // } 
-            // else {
-            //     // button blu
-            //     displayButton = `<a class="btn btn-primary w-100" href="/lotto/${replica.id}">Prenota</a>`;
-            // }
+          
 
             
 
@@ -42,7 +28,7 @@ fetch("/api/repliche?order=asc")
                             <p>Luogo: <b>${replica.rel_evento.rel_locale.luogo}</b></p>
                             <p>Data e ora: <b>${replica.data_ora}</b></p>
                             <p>Posti totali: <b>${replica.rel_evento.rel_locale.posti}</b></p>
-                            <p>Posti prenotati: <b>${replica.rel_prenotazioni.quantita}</b></p>
+                            
                             
 
                             <div class="mt-auto">
@@ -55,6 +41,54 @@ fetch("/api/repliche?order=asc")
         }
     });
 
-    // <p>Prezzo: <b>${lotto.prezzo_unitario} €/${qta_unita_misura}</b></p>
+   
 
-    //  ${displayButton}
+
+    const rowLottiData = document.querySelector('#row-eventi-data');
+
+
+// Fa fetch di un file JSON e lo stampa in console
+// (abbiamo aggiunto un esempio di query string per modificare l'ordinamento)
+fetch("/api/repliche?order=asc")
+    // ......... QUI FLASK STA LAVORANDO PER PREPARARCI LA RISPOSTA
+    // ......... E ALLA FINE CE LA INVIA
+    .then(response => response.json())
+    .then(data => {
+        for (replica of data) {
+            console.log(replica);
+
+
+
+            if (replica.rel_prenotazioni.length > 0) {
+
+                var cont = 0;
+
+                rowLottiData.innerHTML += `
+                    <div class="col-lg-3 my-2">
+                        <div class="card h-100">
+                            
+                            <div class="card-body d-flex flex-column">
+                                
+                                <p>Posti prenotati: <b>${replica.rel_prenotazioni[cont].quantita}</b></p>
+                                
+    
+                                <div class="mt-auto">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    <div>
+                `; 
+                
+                cont += 1;
+
+            }
+               
+
+         
+            
+
+        }
+    });
+
+  
